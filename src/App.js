1fcbb949074e617f62e7bb61234cb0e15bd33cb8
karow <karow.foxx@gmail.com>
2022-04-17 16:06:37 +0100

    added env file, call to notes api, display notes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Note from './Note';
+import { useEffect, useMemo, useState } from 'react';
 
 
 function App() {
+
+  const [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_URL)
+    .then(response => response.json())
+    .then(response => setNotes(response));
+  }, []);
+
+  const calcNotesToShow = (() => {
+    if (notes.length) {
+      return notes.map(n => <Note title={n.title} body={n.body} />);
+    }
+    return <Note title='No notes found!' body='' />;
+  });
+
   return (
     <div>
     <div className='main-navbar main-title'>
       MiNote
     </div>
-    <div className='notes-container'>
-    <Note />
-    </div>
+    {calcNotesToShow()}
     </div>
   );
 }
--- src/Note.js
+++ src/Note.js
@@ -1,15 +1,14 @@
 import './Note.css';
 import {Card} from 'react-bootstrap';
 
-function Note() {
+function Note(props) {
     return (
       <div className='note'>
         <Card style={{ width: '18rem' }}>
           <Card.Body>
-            <Card.Title>Note Title</Card.Title>
+            <Card.Title>{props.title}</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
+              {props.body}
             </Card.Text>
           </Card.Body>
         </Card>
